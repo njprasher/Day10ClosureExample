f3359fa5caba2ed2362d1070dd13920191aeed05
Neeraj Prasher <prasher.neeraj99@gmail.com>
2019-06-19 19:05:59 -0400

    [Neeraj] Examples on Closure

--- Day10ClosureExample/main.swift
+++ Day10ClosureExample/main.swift
@@ -8,5 +8,56 @@
 
 import Foundation
 
-print("Hello, World!")
+func add(a: Int, b: Int) -> Int
+{
+    return a+b
+}
 
+func sub(a: Int, b: Int) -> Int
+{
+    return a-b
+}
+
+let c = add(a: 10, b:20)
+print("c \(c)")
+
+//
+var  funcAdd :(Int, Int) -> Int
+funcAdd = add(a:b:)
+
+let cc = funcAdd(100, 200)
+print("cc \(cc)")
+
+//
+let funcSub = sub(a:b:)
+let s = funcSub(200, 100)
+print("s \(s)")
+
+//
+var mul = {
+    (a: Int, b: Int) -> Int in
+    return a * b
+}
+
+let m = mul(10, 20)
+print("m \(m)")
+
+funcAdd = mul
+print("m \(funcAdd(2, 5))")
+
+let names = ["Chris", "Alex", "Ewa", "Barry", "Daniella"]
+
+func backward(_ s1: String, _ s2: String) -> Bool {
+    return s1 > s2
+}
+
+print(names)
+var reversedNames = names.sorted(by: backward)
+print(reversedNames)
+
+reversedNames = names.sorted(by:
+    {
+        (a: String, b: String) -> Bool in
+        return a < b
+    })
+print(reversedNames)
